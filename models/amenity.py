ee4652631ebec5917b44e18bd52c9c958fdd5068
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:36:46 +0100

    update all classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Defines the Amenity class for HBNB clone"""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenities class definition
+        Attributes:
+            __tablename__
+            name
+            places_amenities
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
