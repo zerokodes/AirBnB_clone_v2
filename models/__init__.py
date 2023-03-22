32a516784ba710d2174e398bc067bc1ca02d5930
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:27:28 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Create a unique storage instance for your application"""
+""" Instatiates a storage object
+    -> If HBNB_TYPE_STORAGE is db:
+        import DBStorage class in file
+        create an instance of DBStorage and store it in the
+        variable storage
+    -> Else:
+        import FileStorage class in file
+        create an instance of FileStorage and store it in the
+        variable storage
+"""
 
-from os import environ
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from os import getenv
 
-# check envirn var to determine storage method
-if environ['HBNB_TYPE_STORAGE'] == 'db':
+# check environ variable to determine storage method
+if getenv('HBNB_TYPE_STORAGE') == 'db':  # DBStorage selected
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
