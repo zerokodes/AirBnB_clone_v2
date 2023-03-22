ff46d1cb9922d2d28e838204a09539209af7e382
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:40:08 +0100

    update all classes

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import models
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base, BaseModel
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
 
 
 class State(BaseModel, Base):
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
             __tablename__ (str): state table
             name (sqlalchemy String): column name(128 chars)
     """
-    __tablename__ = 'state'
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
