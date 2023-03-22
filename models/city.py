262aef4dccf9fb7366143b8ea5343b8bb54d9b2d
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:28:59 +0100

    Delete Object

--- models/city.py
+++ models/city.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+""" Defines the City subclass of the HBNB clone """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name
+        Attributes:
+            __tablename__ (str): represents the table name, cities
+            name (sqlalchemy String): column containing a (128 char) str
+            state_id (salalchemy String): column containig (60 char) str FK.
+
+    """
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
