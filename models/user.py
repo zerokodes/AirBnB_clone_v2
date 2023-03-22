c8a64ddc144bf26012a73901800d19723de49cbd
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:40:50 +0100

    update all classes

--- models/user.py
+++ models/user.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ defines a user class
+        Attributes:
+            __tablename__(str)
+            email(str) 128 chars
+            password(str) 128 chars
+            first_name(srt) 128 chars
+            last_name(str) 128 chars
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
