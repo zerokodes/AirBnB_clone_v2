acba904e01df03bcc0b7abd532b34d430e2f25a5
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:39:22 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" Defines a Review class for HBNB clone """
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review class definition
+        Attributes:
+            __tablename__
+            text
+            place_id
+            user_id
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
