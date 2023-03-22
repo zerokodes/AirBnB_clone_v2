b6259994dbe64493adcf4148f80c8fa9fdda7894
Nduka Henry Chisom <henrysam140@gmail.com>
2023-03-22 17:29:35 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ Defines a State class
+        Attributes:
+            __tablename__ (str): state table
+            name (sqlalchemy String): column name(128 chars)
+    """
+    __tablename__ = 'state'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """get list of related cities"""
+            cities_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    cities_list.append(city)
+            return cities_list
